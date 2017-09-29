a34b0ae9d6248ba9d865303c2896c0d90d5cc76a
oscarlijq <316901017@qq.com>
2017-09-29 21:44:57 +0800

    version2

--- psutil/test2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import psutil
-def on_terminate(proc):
-	print("process {} terminated with exit code {}".format(proc, proc.returncode))
-procs = psutil.Process().children()
-for p in procs:
-	p.terminate()
-	print(p.terminate())
